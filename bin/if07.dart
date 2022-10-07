5361fc1f84d1d884362c66192f53905609f0b169
mehroj <mehrojgaybullayev49@gmail.com>
2022-10-07 12:26:50 +0500

    07

--- bin/if07.dart
+++ bin/if07.dart
@@ -14,6 +14,24 @@ Args:
 Returns:
     str: return answer. 
 */
+String func(int number) {
+  if (number == 1) {
+    return 'Monday';
+  } else if (number == 2) {
+    return 'Tuesday';
+  } else if (number == 3) {
+    return 'Wednesday';
+  } else if (number == 4) {
+    return 'Thursday';
+  } else if (number == 5) {
+    return 'Friday';
+  } else if (number == 6) {
+    return 'Saturday';
+  } else {
+    return 'Sunday';
+  }
+}
 
 void main() {
+  print(func(5));
 }
