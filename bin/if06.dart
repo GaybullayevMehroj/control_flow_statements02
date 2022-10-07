d2131ddd59c16ef75f4e8f446458fa0e8a91c728
mehroj <mehrojgaybullayev49@gmail.com>
2022-10-07 12:26:00 +0500

    06

--- bin/if06.dart
+++ bin/if06.dart
@@ -6,5 +6,25 @@ Args:
 Returns:
     int: return answer. 
 */
+int func(int n) {
+  int a, b, c, d;
+  a = n ~/ 1000;
+  b = n ~/ 100 % 10;
+  c = n % 100 ~/ 10;
+  d = n % 10;
+  if (a > b && a > c && a > d) {
+    return 4;
+  }
+  if (b > a && b > c && b > d) {
+    return 3;
+  }
+  if (c > a && c > b && c > d) {
+    return 2;
+  } else {
+    return 1;
+  }
+}
+
 void main() {
+  print(func(2484));
 }
